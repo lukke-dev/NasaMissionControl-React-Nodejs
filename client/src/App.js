af094885fa761d4cb322b3e70ba5c4b5e20c758f
Lucas <lucas.ffr2014@gmail.com>
2023-02-19 12:49:41 -0300

    Add FrontEnd from Course

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
+import {
+  BrowserRouter as Router,
+} from 'react-router-dom';
+import {
+  Arwes,
+  SoundsProvider,
+  ThemeProvider,
+  createSounds,
+  createTheme,
+} from 'arwes';
+
+import Layout from './pages/Layout';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      AppComponent
-    </div>
-  );
-}
+import { theme, resources, sounds } from './utils';
+
+const App = () => {
+  return <ThemeProvider theme={createTheme(theme)}>
+    <SoundsProvider sounds={createSounds(sounds)}>
+      <Arwes animate background={resources.background.large} pattern={resources.pattern}>
+        {anim => (
+          <Router>
+            <Layout show={anim.entered} />
+          </Router>
+        )}
+      </Arwes>
+    </SoundsProvider>
+  </ThemeProvider>;
+};
 
 export default App;
