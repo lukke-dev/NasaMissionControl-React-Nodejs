6fd4f72e92ee71d880bb442e5abbc773aa4af8cd
Lucas dos Santos <lucas.ffr2014@gmail.com>
2025-02-07 08:01:59 -0300

    ✨ feat(add data to planets model):

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,18 @@
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 8000;
 const http = require("http");
-const app  = require("./app");
+const app = require("./app");
+
+const { loadPlanets } = require("./models/planets.model");
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT} ...`);
-});
+async function startServer() {
+  await loadPlanets();
+
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT} ...`);
+  });
+}
+
+startServer();
